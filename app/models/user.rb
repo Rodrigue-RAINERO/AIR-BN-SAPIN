9cf9b87a5463e36d8543b9556178be946b43a91f
Rodrigue RAINERO <rodriguerainero@gmail.com>
2023-12-08 10:09:02 +0100

    show trees

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
   has_many :trees
   has_many :bookings
   has_many :booked_trees, through: :bookings, source: :tree
